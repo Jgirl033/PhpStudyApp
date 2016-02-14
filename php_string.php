78f3040a0db976401760cbc61517bf4f0c63fe99
Jgirl <18819423747@163.com>
2016-02-14 15:09:23 +0800

    Learn from the website W3School for string.

--- php_string.php
+++ php_string.php
@@ -4,4 +4,25 @@
  * User: jgirl
  * Date: 16-2-14
  * Time: 下午12:26
+ * Description: PHP 数据类型
+ * Url: http://www.w3school.com.cn/php/php_constants.asp
  */
+
+/**
+ * 知识点１：PHP strlen() 函数
+ * strlen() 函数返回字符串的长度，以字符计
+ */
+echo strlen("Hello world!");
+echo "<br>";
+
+
+/**
+ * 知识点２：PHP strpos() 函数
+ * strpos() 函数返回字符串的长度，以字符计
+ */
+echo strpos("Hello world!","world");
+echo "<br>";
+if(!strpos("Hello world!","Jgirl")){
+    echo "False";
+}
+?>
